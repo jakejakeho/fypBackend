339e8890451b34da8a44c6912657120403a31916
hojakejake <mtho@connect.ust.hk>
2018-11-05 21:15:54 +0800

    added ping for testing server online

---
+++ api/routes/ping.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Created'
+    });
+});
+
+module.exports = router;
