2d359efbbd9240938b062e0340fce30fe0ecaf17
hojakejake <mtho@connect.ust.hk>
2019-01-21 16:32:24 +0800

    updated for https server

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const https = require('https');
 
 const app = require('./app');
 const HTTPPort = process.env.HTTP_PORT || 3000;
-const HTTPSPort = process.env.HTTPS_PORT;
+const HTTPSPort = process.env.HTTPS_PORT || 3001;
+
+const options = {
+    cert: fs.readFileSync('./cert/fullchain.pem'),
+    key: fs.readFileSync('./cert/privkey.pem')
+};
 
 const httpServer = http.createServer(app);
 httpServer.listen(HTTPPort, () => {
     console.log('HTTP Server running on port' + HTTPPort);
+});
+
+https.createServer(options, app).listen(HTTPSPort, () => {
+    console.log('HTTPS Server running on port' + HTTPSPort);
 });
