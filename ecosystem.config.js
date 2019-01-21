70f5538d47137884027370d8bbde5a0dafa57865
hojakejake <mtho@connect.ust.hk>
2019-01-21 15:02:03 +0000

    updated envr prodcution

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       host : 'FYPBACKEND.MOOO.COM',
       ref  : 'origin/master',
       repo : 'hojakejake@github.com:fypBackend.git',
-      path : '/etc/fyp/fypBackend',
+      path : '/var/www/fypBackend',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
