b92ec6e74b69dd389db400f2c68fead7ee79a2ff
hojakejake <mtho@connect.ust.hk>
2019-01-23 17:36:02 +0800

    access code lifetime 21days

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const registerRoutes = require('./api/routes/register');
 
 app.oauth = new OAuth2Server({
 	model: require('./oauthModel.js'),
-	accessTokenLifetime: 60 * 60,
+	accessTokenLifetime: 60 * 60 * 24 * 7,
 	allowBearerTokensInQueryString: true
 });
 
