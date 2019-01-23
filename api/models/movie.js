57170d40e808fcf812372e8d1bb45750378e7519
hojakejake <mtho@connect.ust.hk>
2019-01-23 17:10:25 +0800

    updated register and oauth token

--- api/models/movie.js
+++ api/models/movie.js
@@ -4,9 +4,9 @@ const movieSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     movieId: Number,
     title: String,
-    genres:String,
-    poster_path:String,
-    overview:String
+    genres: String,
+    poster_path: String,
+    overview: String
 });
 
 module.exports = mongoose.model('Movie', movieSchema);
