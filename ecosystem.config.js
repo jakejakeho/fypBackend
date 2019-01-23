dec1887d586974a990171498610d2a30c8cc939c
hojakejake <mtho@connect.ust.hk>
2019-01-23 10:48:31 +0800

    DDNS updater added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
-    instances: 4,
+    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
