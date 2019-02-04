a68a3ddbd72bf75a492f12d5f0e5020cc8c26128
hojakejake <mtho@connect.ust.hk>
2019-02-04 21:12:58 +0800

    updated for register and getting app data

---
+++ api/routes/users/register.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.Router();
+const crypto = require('crypto')
+const multer = require('multer');
+var fs = require('fs');
+const mongoose = require('mongoose');
+const User = require('../../models/user')
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const dir1 = './file';
+        if (!fs.existsSync(dir1)) {
+            fs.mkdirSync(dir1);
+        }
+        const dir2 = './file/usericon';
+        if (!fs.existsSync(dir2)) {
+            fs.mkdirSync(dir2);
+        }
+        cb(null, 'file/usericon/');
+    },
+    filename: (req, file, cb) => {
+        let customFileName = crypto.randomBytes(18).toString('hex'),
+            fileExtension = file.originalname.split('.').pop();
+        cb(null, customFileName + '.' + fileExtension)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
+// register
+router.post("/", upload.single('userImage'), (req, res, next) => {
+    console.log(req.file);
+    var userIcon = req.file.path.replace("\\", "/").replace("\\", "/");
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username: req.body.username,
+        password: req.body.password,
+        name: req.body.name,
+        userImage: userIcon
+    });
+    User.findOne({ username: req.body.username })
+        .exec()
+        .then(doc => {
+            if (doc) {
+                doc.password = undefined;
+                console.log("From database" + doc);
+                res.status(500).json({error : "exisited"});
+            } else {
+                user.save()
+                    .then(result => {
+                        console.log(result);
+                        user.password = undefined;
+                        res.status(201).json({
+                            message: 'Handling POST requests to register',
+                            createdUser: user
+                        });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({ error: err });
+                    });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+
+});
+
+module.exports = router;
