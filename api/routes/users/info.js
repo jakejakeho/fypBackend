86aa78e41673b03bf43388c305bf74096de21db2
hojakejake <mtho@connect.ust.hk>
2019-02-06 00:34:47 +0800

    updated minor mistake

--- api/routes/users/info.js
+++ api/routes/users/info.js
@@ -19,7 +19,7 @@ router.get('/', (req, res, next) => {
                     doc.password = undefined;
                     console.log("From database" + doc);
                     if (doc) {
-                        res.status(200).json(doc.profile);
+                        res.status(200).json(doc.profile[0]);
                     } else {
                         res.status(404).json({
                             message: 'No valid entry found for provided ID'
