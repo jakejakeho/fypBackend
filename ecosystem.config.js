0583c1ad70aee3f4746ab05b6881c846e857fbd6
hojakejake <mtho@connect.ust.hk>
2019-04-29 18:06:04 +0800

    updated instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,7 @@ module.exports = {
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
-    instances: max,
-	exec_mode: "cluster" ,
+    instances: 4,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
